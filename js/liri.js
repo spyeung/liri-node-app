b14f3ca9304f93ffc37a1566c7ae68896dc7fe7c
Patrick <patrick.yeung7@gmail.com>
2019-10-27 18:06:03 -0400

    tracking

---
+++ js/liri.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const axios = require('axios');
+
+var keys = require("./keys.js");
+
+var Spotify = require('keys.spotify');
+ 
+function concert_this(){
+
+}
+
+function spotify_this_song(){
+
+}
+
+function movie_this(){
+
+}
+
+function do_what_it_says(){
+
+}
